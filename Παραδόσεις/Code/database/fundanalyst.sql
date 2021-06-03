eb595e4b7f950a4b34c8b2a627bf98201f11f0e9
Excel3884 <angelosdimoglis@gmail.com>
2021-06-03 12:38:24 +0300

    Added expenses tables to database

--- Παραδόσεις/Code/database/fundanalyst.sql
+++ Παραδόσεις/Code/database/fundanalyst.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: May 07, 2021 at 03:03 PM
--- Server version: 5.7.31
--- PHP Version: 7.3.21
+-- Host: 127.0.0.1
+-- Generation Time: Jun 03, 2021 at 11:36 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,14 +27,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `accounts`
 --
 
-DROP TABLE IF EXISTS `accounts`;
-CREATE TABLE IF NOT EXISTS `accounts` (
+CREATE TABLE `accounts` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL,
-  `owner` tinyint(1) NOT NULL,
-  PRIMARY KEY (`username`)
+  `owner` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -47,20 +45,68 @@ INSERT INTO `accounts` (`username`, `password`, `name`, `surname`, `owner`) VALU
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `name` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL,
+  `salary` float NOT NULL,
+  `month` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`name`, `lastname`, `id`, `salary`, `month`) VALUES
+('Elias', 'Dimitriou', 1, 1000, ''),
+('Iraklis', 'Ioannidis', 2, 1600, ''),
+('Prokopis', 'Iordanou', 3, 1200, ''),
+('Paraskevi', 'Dimitriou', 4, 1200, ''),
+('Vasilis', 'Papageorgiou', 5, 1800, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `operatingcosts`
+--
+
+CREATE TABLE `operatingcosts` (
+  `power` float NOT NULL,
+  `water` float NOT NULL,
+  `phone` float NOT NULL,
+  `rent` float NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `operatingcosts`
+--
+
+INSERT INTO `operatingcosts` (`power`, `water`, `phone`, `rent`, `date`) VALUES
+(143.32, 115.22, 9469450000, 350, '2021-06-14'),
+(69.96, 61.13, 6988210000, 300, '2021-06-18'),
+(93.61, 173.57, 6858840000, 320, '2021-06-10'),
+(161.3, 93.15, 6939720000, 400, '2021-06-24'),
+(134.55, 76.91, 6854770000, 350, '2021-06-12');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `sales`
 --
 
-DROP TABLE IF EXISTS `sales`;
-CREATE TABLE IF NOT EXISTS `sales` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `sales` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `type` varchar(255) NOT NULL,
   `date` date NOT NULL,
   `price` float NOT NULL,
   `amount` int(255) NOT NULL,
-  `purchase` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`,`date`)
-) ENGINE=InnoDB AUTO_INCREMENT=240 DEFAULT CHARSET=utf8;
+  `purchase` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `sales`
@@ -73,6 +119,66 @@ INSERT INTO `sales` (`id`, `name`, `type`, `date`, `price`, `amount`, `purchase`
 (237, 'PSU EVGA 400W', 'Hardware', '2021-05-11', 29.9, 4, 'Physical Shop'),
 (238, 'Logitech MX Ergo Mouse', 'Peripheral', '2021-05-17', 90, 6, 'Physical Shop'),
 (239, 'Microsoft Office 2019 Pro', 'Software', '2021-05-17', 49.99, 10, 'e-shop');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplies`
+--
+
+CREATE TABLE `supplies` (
+  `cost` varchar(255) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `supplies`
+--
+
+INSERT INTO `supplies` (`cost`, `date`) VALUES
+('267.29', '2021-06-08'),
+('462.28', '2021-06-03'),
+('483.89', '2021-06-29'),
+('532.53', '2021-06-16'),
+('527.80', '2021-06-20');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id`,`date`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=240;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
